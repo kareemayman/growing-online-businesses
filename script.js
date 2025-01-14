a45da953cc758de02c7b931a284c2eabceb8b8f3
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-01-14 11:51:46 +0200

    Added Scroll-To-Top Button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const burgerIcon = document.querySelector('header .burger-icon')
 const mobileMenu = document.querySelector('.mobile-menu')
 
+const heroSection = document.querySelector('main .hero')
+const scrollToTopButton = document.querySelector('.scroll-to-top')
+
 document.addEventListener('click', e => {
 
     // Show or Hide Menu When Clicking On Burger Icon Or Mobile Menu Links
@@ -16,4 +19,25 @@ document.addEventListener('click', e => {
         mobileMenu.classList.remove('show-mobile-menu')
         document.body.classList.remove('disable-scrolling')
     }
+})
+
+// Event For Showing Tracking Scroll Position & Showing Scroll-To-Top Button
+document.addEventListener('scroll', e => {
+
+    let scrollPos = window.scrollY
+
+    if (scrollPos >= (heroSection.offsetTop)) {
+        scrollToTopButton.classList.add('show-scroll-button')
+    } else {
+        scrollToTopButton.classList.remove('show-scroll-button')
+    }
+})
+
+// Scroll Button Click Event
+scrollToTopButton.addEventListener('click', e => {
+
+    window.scroll({
+        top: 0,
+        behavior: "smooth",
+    })
 })
